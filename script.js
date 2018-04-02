841d5fa640f7575be8517758011d5749a6c47c5b
Hoangyen256 <36033659+Hoangyen256@users.noreply.github.com>
2018-04-02 13:03:10 +0700

    1

--- script.js
+++
@@ -1,7 +0,0 @@
-//ham su ly de lay video tu youtube               
-$(document).ready(function(){
-    $('.list-video li').click(function(){
-        $(this).parent().siblings('.embed-player').attr('src','http://www.youtube.com/embed/'+$(this).children('a').attr('title'));                                     
-    });
-});
-					                    
